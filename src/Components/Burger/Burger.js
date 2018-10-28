e16d4349a23d6dca5d788d34fe53493faa32ecd3
Carlo Bruno <carlofbruno@gmail.com>
2018-10-28 07:47:34 -0700

    lecture 120

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,12 +3,25 @@ import React from "react";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
-const Burger = () => {
+const Burger = props => {
+  const ingredientArray = Object.keys(props.ingredients).map(
+    type => {
+      return [...Array(props.ingredients[type])].map(
+        (element, i) => {
+          console.log(type, element, i);
+          // we get 'type' from Object.keys (ln.7-8)
+          // element doesnt matter, but we need i (index)
+          // for unique key & to know how many element we have
+          return <BurgerIngredient key={type + i} type={type} />;
+        }
+      );
+    }
+  );
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientArray}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
