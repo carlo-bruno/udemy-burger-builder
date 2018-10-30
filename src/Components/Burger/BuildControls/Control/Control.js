7564a7a206e26c317f130de63ea4963554ab5c12
Carlo Bruno <carlofbruno@gmail.com>
2018-10-29 22:12:21 -0700

    lecture 125

--- src/Components/Burger/BuildControls/Control/Control.js
+++ src/Components/Burger/BuildControls/Control/Control.js
@@ -5,7 +5,13 @@ const Control = props => {
   return (
     <div className={classes.Control}>
       <div className={classes.Label}>{props.label}</div>
-      <button className={classes.Less}>Less</button>
+      <button
+        className={classes.Less}
+        onClick={props.removed}
+        disabled={props.disabled}
+      >
+        Less
+      </button>
       <button className={classes.More} onClick={props.added}>
         More
       </button>
