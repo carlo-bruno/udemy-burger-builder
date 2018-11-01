7e652bc06989d50e2b917486a026e128cdb3a5b4
Carlo Bruno <carlofbruno@gmail.com>
2018-10-31 17:37:59 -0700

    lecture 128

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../Components/Burger/Burger";
 import BuildControls from "../../Components/Burger/BuildControls/BuildControls";
+import Modal from "../../Components/UI/Modal/Modal";
+import OrderSummary from "../../Components/Burger/OrderSummary/OrderSummary";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -78,6 +80,10 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal>
+          {" "}
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
