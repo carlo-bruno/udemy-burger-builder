58062db3103ca64d09d40284fb13a6eac978d653
Carlo Bruno <carlofbruno@gmail.com>
2018-11-01 22:27:14 -0700

    lecture 135

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
 
 const Toolbar = () => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <div>LOGO</div>
-      <nav>{/* <ul></ul> */}</nav>
+      <Logo />
+      <nav>
+        <ul>...</ul>
+      </nav>
     </header>
   );
 };
